eb57fb7013754130ac4c6f2642b83b69b49cda0d
Eduardo Castilho <developer.ecb@gmail.com>
2025-04-02 12:54:31 -0300

    ajusta importações server.ts

--- back/server.ts
+++ back/server.ts
@@ -1,24 +1,24 @@
-import fastify, { FastifyReply, FastifyRequest } from "fastify";
+import fastify from "fastify";
 import { authMiddleware } from "./src/middleware";
 
-import cors from '@fastify/cors'
+import cors from '@fastify/cors';
 
 const app = fastify()
 
 app.register(cors, { origin: '*' })
 
 app.get('/', (request, reply) => {
-    reply.send('pong');
+	reply.send('pong');
 });
 
 app.get('/protected', { preHandler: authMiddleware }, (request, reply) => {
-    return reply.status(200).send({ message: 'User authorized!' })
+	return reply.status(200).send({ message: 'User authorized!' })
 })
 
 app.listen({ port: 3030, host: '127.0.0.1' }, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
+	if (err) {
+		console.error(err)
+		process.exit(1)
+	}
+	console.log(`Server listening at ${address}`)
 })
