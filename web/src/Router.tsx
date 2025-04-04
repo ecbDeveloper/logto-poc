daa7b8a5d983740274ff5417711339c3f68d5355
ecbDeveloper <developer.ecb@gmail.com>
2025-04-04 00:23:11 -0300

    padroniza nome dos arquivos do front

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
-import { Callback } from "./pages/Callback/Callback";
-import { UserAuthorization } from "./pages/UserAuthorization/UserAuthorization";
+import { Home } from "./pages/Home";
+import { Callback } from "./pages/Callback";
+import { UserAuthorization } from "./pages/UserAuthorization";
 
 export function Router() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/callback" element={<Callback />} />
-            <Route path="/user" element={<UserAuthorization />} />
-        </Routes>
-    )
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/callback" element={<Callback />} />
+			<Route path="/user" element={<UserAuthorization />} />
+		</Routes>
+	)
 }
